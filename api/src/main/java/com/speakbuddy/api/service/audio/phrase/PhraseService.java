b726d2b0bb6290fc667175bc00c2ae78decdd9ea
Wisnu Wicaksono <wisnu.wicaksono@traveloka.com>
2024-12-01 10:05:33 +0700

    [checkpoint] save wav, get as aiff

--- api/src/main/java/com/speakbuddy/api/service/audio/phrase/PhraseService.java
+++ api/src/main/java/com/speakbuddy/api/service/audio/phrase/PhraseService.java
@@ -6,23 +6,33 @@ import com.speakbuddy.api.database.repository.entity.PhraseEntity;
 import com.speakbuddy.api.database.repository.entity.ids.PhraseIds;
 import com.speakbuddy.api.exception.BadRequestException;
 import com.speakbuddy.api.exception.FileProcessorException;
+import com.speakbuddy.api.exception.InternalServerError;
 import com.speakbuddy.api.utility.FileUtility;
 import com.speakbuddy.api.utility.impl.LocalFileProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.List;
 
+import javax.print.attribute.standard.Media;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import static org.apache.commons.io.FilenameUtils.getBaseName;
 import static org.apache.commons.io.FilenameUtils.getExtension;
@@ -63,22 +73,186 @@ public class PhraseService {
   }
 
   public ResponseEntity<StreamingResponseBody> getPhrase(String userId, String phraseId, String audioFormat) {
+    if (!List.of("aiff", "au").contains(audioFormat)) {
+      throw new BadRequestException("Audio file format not supported yet");
+    }
+
     final PhraseEntity existPhraseEntity = phraseManager.findById(new PhraseIds(userId, phraseId));
 
     final String fileName = Paths.get(existPhraseEntity.getFilePath()).getFileName().toString();
     final String fileNameWithoutExtension = getBaseName(fileName);
 
+    final File myAudioFile = Paths.get("..", "audio", existPhraseEntity.getFilePath()).toFile();
+
+    final StreamingResponseBody responseBody = outputStream -> this.handleStreamingResponse(outputStream, myAudioFile);
+
+    final String headerValue = String.format("attachment; filename=%s.%s", fileNameWithoutExtension, audioFormat);
+
     return ResponseEntity.ok()
-        //.header(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=%s.%s", fileNameWithoutExtension, audioFormat))
-        //.contentType(MediaType.APPLICATION_OCTET_STREAM)
-        .body(os -> {
-          try (InputStream inputStream = fileUtility.getFile(existPhraseEntity.getFilePath())) {
-            inputStream.transferTo(os);
-          } catch (IOException e) {
-            log.error("[getFile] fail read file with path: {}", "filePath", e);
-            throw new FileProcessorException("File read failed");
-          }
-        });
+        .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+        .contentType(MediaType.parseMediaType(String.format("audio/%s", audioFormat.toLowerCase())))
+        .body(responseBody);
+  }
+
+  private void handleStreamingResponse(OutputStream outputStream, File file) {
+    try (AudioInputStream wavStream = AudioSystem.getAudioInputStream(file)) {
+      wavStream.reset();
+      AudioSystem.write(wavStream, AudioFileFormat.Type.AIFF, outputStream);
+      outputStream.flush();
+    } catch (UnsupportedAudioFileException e) {
+      log.error("UnsupportedAudioFileException. file: {}. err: {}", file.getAbsoluteFile(), e.getMessage(), e);
+      throw new InternalServerError("Audio file not supported");
+    } catch (IOException e) {
+      log.error("IOException. file: {}. err: {}", file.getAbsoluteFile(), e.getMessage(), e);
+      throw new InternalServerError("IO error");
+    }
+  }
+
+  public ResponseEntity<StreamingResponseBody> testya(String userId, String phraseId, String audioFormat) {
+    //final PhraseEntity existPhraseEntity = phraseManager.findById(new PhraseIds(userId, phraseId));
+
+    //final String fileName = Paths.get(existPhraseEntity.getFilePath()).getFileName().toString();
+    //final String fileNameWithoutExtension = getBaseName(fileName);
+
+    // ....
+    //final File myAudioFile = Paths.get("..", "audio", existPhraseEntity.getFilePath()).toFile();
+    //final File myAudioFile = new File("/Users/wicaksno/code/speakbuddy/wisnu/audio/" + existPhraseEntity.getFilePath());
+    final File myAudioFile = new File("/Users/wicaksno/code/speakbuddy/wisnu/audio/9ba2b8a1-ad74-41fc-8d75-ad97eca89f41/testid/1732805255893.wav");
+
+    try (AudioInputStream wavStream = AudioSystem.getAudioInputStream(myAudioFile)) {
+      AudioFileFormat inFileFormat = AudioSystem.getAudioFileFormat(myAudioFile);
+      if (inFileFormat.getType() != AudioFileFormat.Type.AIFF) {
+        System.out.println("Not AIFF");
+      }
+      //wavStream.reset();
+
+      //if(AudioSystem.isFileTypeSupported(AudioFileFormat.Type.AIFF, wavStream)){
+      System.out.println("DO CONVERT!");
+      //AudioSystem.write(wavStream, AudioFileFormat.Type.AIFF, outFile);
+      File outFile = new File("/Users/wicaksno/code/speakbuddy/wisnu/audio/9ba2b8a1-ad74-41fc-8d75-ad97eca89f41/testid/1732805255893.aiff");
+      AudioSystem.write(wavStream,
+          AudioFileFormat.Type.AIFF, outFile);
+
+      final StreamingResponseBody responseBody = outputStream -> {
+        AudioSystem.write(wavStream, AudioFileFormat.Type.AIFF, outputStream);
+        //try (AudioInputStream aiffStream = AudioSystem.getAudioInputStream(aiffFormat, pcmStream)) {
+        //  byte[] buffer = new byte[4096];
+        //  int bytesRead;
+        //  while ((bytesRead = aiffStream.read(buffer)) != -1) {
+        //    outputStream.write(buffer, 0, bytesRead);
+        //  }
+        //}
+      };
+
+      return ResponseEntity.ok()
+          .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=hehehehe.aiff")
+          .contentType(MediaType.parseMediaType("audio/wav"))
+          .body(responseBody);
+      //}
+      //
+      //AudioFormat targetFormat = new AudioFormat(
+      //    AudioFormat.Encoding.PCM_SIGNED,
+      //    sourceFormat.getSampleRate(),
+      //    16,
+      //    sourceFormat.getChannels(),
+      //    sourceFormat.getChannels() * 2,
+      //    sourceFormat.getSampleRate(),
+      //    false
+      //);
+      //
+      //AudioInputStream pcmStream = AudioSystem.getAudioInputStream(targetFormat, wavStream);
+
+      //AudioFormat aiffFormat = new AudioFormat(
+      //    AudioFormat.Encoding.PCM_SIGNED,
+      //    sourceFormat.getSampleRate(),
+      //    sourceFormat.getSampleSizeInBits(),
+      //    sourceFormat.getChannels(),
+      //    sourceFormat.getFrameSize(),
+      //    sourceFormat.getSampleRate(),
+      //    sourceFormat.isBigEndian()
+      //);
+
+      //StreamingResponseBody responseBody = outputStream -> {
+      //  try (AudioInputStream aiffStream = AudioSystem.getAudioInputStream(aiffFormat, pcmStream)) {
+      //    byte[] buffer = new byte[4096];
+      //    int bytesRead;
+      //    while ((bytesRead = aiffStream.read(buffer)) != -1) {
+      //      outputStream.write(buffer, 0, bytesRead);
+      //    }
+      //  }
+      //};
+
+      //byte[] buffer = wavStream.readAllBytes();
+
+      //return ResponseEntity.ok()
+      //    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=hehehehe.wav")
+      //    .contentType(MediaType.parseMediaType("audio/wav"))
+      //    .body(buffer);
+
+      //try (AudioInputStream aiffStream = AudioSystem.getAudioInputStream(aiffFormat, wavStream)) {
+      //  byte[] buffer = wavStream.readAllBytes();
+      //
+      //  return ResponseEntity.ok()
+      //      .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=test.aiff")
+      //      .contentType(MediaType.APPLICATION_OCTET_STREAM)
+      //      .body(buffer);
+      //}
+
+
+      //return ResponseEntity.ok()
+      //    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=test.aiff")
+      //    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+      //    .body(responseBody);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return ResponseEntity.status(500).build();
+    }
+
+
+    // ...
+
+    /**
+     try {
+     final InputStream inputStream = fileUtility.getFile(existPhraseEntity.getFilePath());
+     StreamingResponseBody responseBody = outputStream -> {
+
+     int numberOfBytesToWrite;
+     byte[] data = new byte[1024];
+     while ((numberOfBytesToWrite = inputStream.read(data, 0, data.length)) != -1) {
+     System.out.println("Writing some bytes..");
+     outputStream.write(data, 0, numberOfBytesToWrite);
+     }
+
+     inputStream.close();
+     };
+     return ResponseEntity.ok()
+     .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=test.wav")
+     .contentType(MediaType.APPLICATION_OCTET_STREAM)
+     .body(responseBody);
+     } catch (IOException e) {
+     e.printStackTrace();  // TODO impl
+     }
+
+
+
+
+
+     //return new ResponseEntity<StreamingResponseBody>
+     //    (responseStream, responseHeaders, HttpStatus.PARTIAL_CONTENT);
+
+     return ResponseEntity.ok()
+     //.header(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=%s.%s", fileNameWithoutExtension, audioFormat))
+     .contentType(MediaType.valueOf("audio/wav"))
+     .body(os -> {
+     try (InputStream inputStream = fileUtility.getFile(existPhraseEntity.getFilePath())) {
+     inputStream.transferTo(os);
+     } catch (IOException e) {
+     log.error("[getFile] fail read file with path: {}", "filePath", e);
+     throw new FileProcessorException("File read failed");
+     }
+     });
+     **/
   }
 
   private String constructDirectory(String userId, String phraseId) {
--- api/src/main/java/com/speakbuddy/api/utility/impl/LocalFileProcessor.java
+++ api/src/main/java/com/speakbuddy/api/utility/impl/LocalFileProcessor.java
@@ -1,6 +1,7 @@
 package com.speakbuddy.api.utility.impl;
 
 import com.speakbuddy.api.exception.BadRequestException;
+import com.speakbuddy.api.exception.EntityNotFoundException;
 import com.speakbuddy.api.exception.FileProcessorException;
 import com.speakbuddy.api.utility.FileUtility;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -15,6 +17,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -35,7 +38,7 @@ public class LocalFileProcessor implements FileUtility {
     }
 
     final String aliasPath = String.format("%s/%s", targetPath, targetFileName);
-    final Path destinationPath = Paths.get("..", relativePath, aliasPath).normalize();
+    final Path destinationPath = Paths.get("..", relativePath, targetPath).normalize();
 
     try {
       Files.createDirectories(destinationPath.toAbsolutePath());
@@ -44,18 +47,59 @@ public class LocalFileProcessor implements FileUtility {
       throw new FileProcessorException("Directory creation failed");
     }
 
-    try (InputStream inputStream = file.getInputStream()) {
-      // https://stackoverflow.com/questions/5529754/java-io-ioexception-mark-reset-not-supported
-      //final InputStream bufferedIn = new BufferedInputStream(inputStream);
-      //final AudioInputStream originalAudioStream = AudioSystem.getAudioInputStream(bufferedIn);
-      //AudioSystem.write(originalAudioStream, AudioFileFormat.Type.WAVE, destinationPath.toFile());
-      Files.copy(inputStream, destinationPath.toAbsolutePath(), StandardCopyOption.REPLACE_EXISTING);
+    File outputFile = destinationPath.resolve(targetFileName).toFile();
+
+    try (InputStream inputStream = file.getInputStream();
+         BufferedInputStream bis = new BufferedInputStream(inputStream);
+         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bis)) {
+
+      //audioInputStream.reset();
+      //AudioFileFormat hehehe = AudioSystem.getAudioFileFormat(file.getInputStream());
+      //System.out.println(hehehe.getType());
+
+      AudioFormat sourceFormat = audioInputStream.getFormat();
+      AudioFormat targetFormat = new AudioFormat(
+          AudioFormat.Encoding.PCM_SIGNED,
+          sourceFormat.getSampleRate(),
+          16,
+          sourceFormat.getChannels(),
+          sourceFormat.getChannels() * 2,
+          sourceFormat.getSampleRate(),
+          false
+      );
+
+      try (AudioInputStream wavStream = AudioSystem.getAudioInputStream(targetFormat, audioInputStream)) {
+        AudioSystem.write(wavStream, AudioFileFormat.Type.WAVE, outputFile);
+      }
+
+      //try (;
+      //     FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+      //  byte[] buffer = new byte[4096];
+      //  int bytesRead;
+      //  while ((bytesRead = audioInputStream.read(buffer)) != -1) {
+      //    fileOutputStream.write(buffer, 0, bytesRead);
+      //  }
+    } catch (UnsupportedAudioFileException e) {
+      log.error("Error processing audio file", e);
+      throw new FileProcessorException("Error file creation. Err: " + e.getMessage());
     } catch (IOException e) {
-      log.error("[storeFile] fail save data with target: {}", destinationPath, e);
-      throw new FileProcessorException("File save failed");
+      log.error("Error read audio file", e);
+      throw new FileProcessorException("Error read audio file. Err: " + e.getMessage());
+    }
+
+
+    //try (InputStream inputStream = file.getInputStream()) {
+    // https://stackoverflow.com/questions/5529754/java-io-ioexception-mark-reset-not-supported
+    //final InputStream bufferedIn = new BufferedInputStream(inputStream);
+    //final AudioInputStream originalAudioStream = AudioSystem.getAudioInputStream(bufferedIn);
+    //AudioSystem.write(originalAudioStream, AudioFileFormat.Type.WAVE, destinationPath.toFile());
+    //  Files.copy(inputStream, destinationPath.toAbsolutePath(), StandardCopyOption.REPLACE_EXISTING);
+    //} catch (IOException e) {
+    //  log.error("[storeFile] fail save data with target: {}", destinationPath, e);
+    //  throw new FileProcessorException("File save failed");
     //} catch (UnsupportedAudioFileException e) {
     //  e.printStackTrace();  // TODO impl
-    }
+    //}
 
     return aliasPath;
   }
